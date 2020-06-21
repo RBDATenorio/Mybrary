775057d49ec2e9c33071a57d68f90b69739d4f6e
Ricardo Tenorio <ricardobatenorio@gmail.com>
2020-06-20 23:33:38 -0300

    Books create and books index

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-if(process.env.NODE_ENV !== 'production') {
+/* if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config({ path: '.env' })
-  }
+} */
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorsRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
+
 
 const mongoose = require('mongoose');
 
@@ -22,13 +27,25 @@ app.use(express.static('public'));
 // connection with database
 //mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
-mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex : true})
+/* mongoose.connect(process.env.DATABASE_URL, {
+  useUnifiedTopology: true, 
+  useNewUrlParser: true, 
+  useCreateIndex : true});
 
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose')); */
+
+mongoose.connect(process.env.DATABASE_URL, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
 app.use('/authors', authorsRouter);
+app.use('/books', bookRouter);
+
 
 app.listen(process.env.PORT || 8484);
